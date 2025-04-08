61631c8e40e53d64470917bc62640805dceb9131
K. Pramadu Shehan Chithakshana <151552571+Pramadu2001@users.noreply.github.com>
2025-04-08 14:09:26 +0530

    back-end setup

---
+++ CrafNest_Server/src/main/java/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "dgmfmdmecmesmedmd";
+    public static final String JWT_Header = "Authorization";
+
+}
---
+++ CrafNest_Server/src/test/java/com/spring/crafnest_server/CrafNestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.crafnest_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrafNestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
