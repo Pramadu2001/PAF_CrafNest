b7b8056210d9758fdffe6820778b0f8be7df5c7d
Sun3ra <162122255+Sun3ra@users.noreply.github.com>
2025-05-06 15:37:48 +0530

    Delete CrafNest_Server directory

--- CrafNest_Server/src/main/java/config/JwtConstant.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public class JwtConstant {
-    public static final String SECRET_KEY = "dgmfmdmecmesmedmd";
-    public static final String JWT_Header = "Authorization";
-
-}
--- CrafNest_Server/src/main/java/exception/CraftPostException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class CraftPostException extends Exception{
-
-    public CraftPostException(String message) {
-        super(message);
-    }
-}
